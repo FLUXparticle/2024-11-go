9efd2ecae3429860a971aea40a97b052d0e64587
Sven Reinck <sreinck@fluxparticle.de>
2024-11-06 09:55:16 +0100

    SQLite

--- regex/database.go
+++ regex/database.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 )
 
+func setupDatabase() *sql.DB {
+	// Alte Datenbankdatei löschen, falls sie existiert
+	if _, err := os.Stat("./cocktails.db"); err == nil {
+		err = os.Remove("./cocktails.db")
+		if err != nil {
+			log.Fatal("Fehler beim Löschen der alten Datenbank:", err)
+		}
+	}
+
+	// SQLite-Datenbank erstellen
+	db, err := sql.Open("sqlite3", "./cocktails.db")
+	if err != nil {
+		log.Fatal("Fehler beim Öffnen der Datenbank:", err)
+	}
+
+	// Tabellen erstellen
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS cocktails (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS ingredients (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cocktail_id INTEGER,
+            amount TEXT,
+            name TEXT,
+            FOREIGN KEY (cocktail_id) REFERENCES cocktails(id)
+        );
+    `)
+	if err != nil {
+		log.Fatal("Fehler beim Erstellen der Tabellen:", err)
+	}
+
+	return db
+}
+
 func insertIntoDatabase(allCocktails []*Cocktail) {
+	db := setupDatabase()
+	defer db.Close()
+
 	// TODO
 }
